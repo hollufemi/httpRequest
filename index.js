07bbb6d32c29917314fa0cf66d2be55af9febf7e
holluville <femolola@gmail.com>
2024-11-14 17:08:03 +0100

    hh

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mongoose =require("mongoose");
 
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 2020;
+mongoose .connect("mongodb+srv://fahakintayo:VM7RS9Z13JYcAL5w@cluster0.nli88.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=> console.log("DB connected"))
+.catch(err=> console.log(err))
+
+const childSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    age:{
+        type: Number,
+        require: true
+    },
+    stateoforigin:{
+        type: String
+    }
+})
+
+const userModel= new mongoose.model("Children",childSchema)
+
 
 
 console.log(2+2)
@@ -22,4 +42,39 @@ app.post("/signup", (request,response)=>{
     response.send("Request Received Successfully")
 })
 
+app.post("/addnewchild", (request,response)=>{
+    console.log(request.body);
+    const newChild = new userModel();
+    newChild.name = request.body.name;
+    newChild.age = request.body.age;
+    newChild.stateoforigin = request.body.stateoforigin;
+
+    newChild.save()
+    .then(() => response.send("Child Registartion Successful"))
+    .catch(err=>{
+        console.log(err);
+        
+        response.send("child could not be added");
+        
+    })
+})
+
+app.get("/allchildren", (request,response)=>{
+    userModel.find()
+    .then(data=>{
+        console.log(data)
+        response.json({
+            message:"Data found",
+            data
+        })
+    })
+        .catch(err=>{
+            console.log(err)
+            response.send("An error occured")
+        })
+   
+
+})
+
+const port = process.env.PORT || 2020;
 app.listen(port, console.log("I am running on port", port))
